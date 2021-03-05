f37868e70d284af2cd729f8c748c8f02837b3ed2
Thomas Dost <dostthomas@icloud.com>
2021-03-05 17:35:21 +0100

    iupac to char tests

--- tests/test_iupac.nim
+++ tests/test_iupac.nim
@@ -68,5 +68,77 @@ proc testIupac()=
       r = '?'.toNucleotide
       check r == 0b0000_0010
 
+      #----------------------------------------------
+      #Iupac to char tests
+      #----------------------------------------------
+      var n: Nucleotide = Nucleotide(136)
+      var c = n.toChar
+      check c == 'A'
+
+      n = Nucleotide(72)
+      c = n.toChar
+      check c == 'G'
+      
+      n = Nucleotide(40)
+      c = n.toChar
+      check c == 'C'
+      
+      n = Nucleotide(24)
+      c = n.toChar
+      check c == 'T'
+
+      n = Nucleotide(192)
+      c = n.toChar
+      check c == 'R'
+      
+      n = Nucleotide(160)
+      c = n.toChar
+      check c == 'M'
+      
+      n = Nucleotide(144)
+      c = n.toChar
+      check c == 'W'
+      
+      n = Nucleotide(96)
+      c = n.toChar
+      check c == 'S'
+      
+      n = Nucleotide(80)
+      c = n.toChar
+      check c == 'K'
+      
+      n = Nucleotide(48)
+      c = n.toChar
+      check c == 'Y'
+
+      n = Nucleotide(224)
+      c = n.toChar
+      check c == 'V'
+
+      n = Nucleotide(176)
+      c = n.toChar
+      check c == 'H'
+
+      n = Nucleotide(208)
+      c = n.toChar
+      check c == 'D'
+
+      n = Nucleotide(112)
+      c = n.toChar
+      check c == 'B'
+
+
+      n = Nucleotide(240)
+      c = n.toChar
+      check c == 'N'
+
+      n = Nucleotide(4)
+      c = n.toChar
+      check c == '-'
+
+      n = Nucleotide(2)
+      c = n.toChar
+      check c == '?'
+
 testIupac()
 
