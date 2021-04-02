a80bd8a3befaede1c586bcdd8afb083adfbfed8f
Thomas Dost <dostthomas@icloud.com>
2021-04-02 09:50:52 +0200

    Renamed proc

--- tests/test_fasta_writing.nim
+++ tests/test_fasta_writing.nim
@@ -5,13 +5,13 @@ proc fastaWriting()=
     test "non-multiline, <80 charactes":
       let test = ">1\nACTG\n>2\nAAAA"
       let r: Alignment = parseFastaAlignmentString(test)
-      let s: string = write_fasta_string(r, false)
+      let s: string = writeFastaAlignmentString(r, false)
       check test == s
 
     test "multiline, <80 charactes":
       let test = ">1\nACTG\n>2\nAAAA"
       let r: Alignment = parseFastaAlignmentString(test)
-      let s: string = write_fasta_string(r, true)
+      let s: string = writeFastaAlignmentString(r, true)
       check test == s
 
 fastaWriting()
