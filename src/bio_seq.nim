ff5427cf7ea899f1c7d5d785c8b0c84410050c7a
Thomas Dost <dostthomas@icloud.com>
2021-04-04 12:25:36 +0200

    Fastq parsing nearly works,
    Maybe thee is a bug in the phred score check or our test file is not
    phred+33

--- src/bio_seq.nim
+++ src/bio_seq.nim
@@ -1,5 +1,8 @@
 import ./bio_seq/iupac_uint8
 import ./bio_seq/io/fasta
+import ./bio_seq/io/fastaq
 
 export iupac_uint8,
-       fasta
+       fasta,
+       fastq
+       
--- src/bio_seq/io/fastq.nim
+++ src/bio_seq/io/fastq.nim
@@ -1,12 +1,18 @@
 import zip/gzipfiles
 import ../iupac_uint8
+import sequtils
+import strutils
+import sugar
 ## Sources used in the writing of this parser
 ## For a basic introduction look at
 ## http://homer.ucsd.edu/homer/basicTutorial/fastqFiles.html
 ## For different types of FastQ files look at
 ## https://www.ncbi.nlm.nih.gov/sra/docs/submitformats/#fastq-files 
-##
+## For phred scores look at 
+## http://drive5.com/usearch/manual/quality_score.html
 
+# TODO
+# Add line and colum information for errors
 
 type 
   Record = ref object
@@ -18,11 +24,23 @@ type
     ## Second header, must start with a + but can be empty, even then the + is still required 
     quality: string
     ## Phred score for the respecitve nucleotides must be the same length, as `nucs`
+    ## Atm. the moment on
 
   FastQ = ref object
     sequences: seq[Record]
 
   FastQError* = object of CatchableError
+
+proc `$`*(s: Record): string = 
+  let h =  s.header
+  var n: string = map(s.nucs, toChar).map((c: char) => $c).join
+  let sh = s.secHeader
+  let q = s.quality
+  result = h & "\n" & n & "\n" & sh & "\n" & q & "\n"
+
+proc `$`*(s: var FastQ): string = 
+  for r in s.sequences:
+    result.add($r)
 
 proc parseHeader(header: string): string =
   if header == "":
@@ -41,6 +59,12 @@ proc parseSecHeader(header: string): string =
     raise newException(FastQError, "Second header must start with a +")
   header[1 .. ^1]
 
+proc parseQuality(qual: string): string =
+  for q in qual:
+    #if int(q) < 33 or int(q) > 75:
+      #raise newException(FastQError, "Unsopported phred score")
+    #else:
+    result.add(q)
 
 
 proc parseFastQFile(filepath: string): FastQ =
@@ -58,13 +82,22 @@ proc parseFastQFile(filepath: string): FastQ =
     let n = parseNucleotides(l)
     l = gzfs.readLine()
     let sc = parseSecHeader(l)
-
+    l = gzfs.readLine()
+    let q = parseQuality(l)
+    if q.len != n.len:
+      raise newException(FastQError, "Nucleotide sequence and quality line have different length")
+    else:
+      var r: Record
+      new(r)
+      r.header = h
+      r.nucs = n
+      r.secHeader = sc
+      r.quality = q
+      fastQ.sequences.add(r)
+  fastQ
 
 when isMainModule:
-  let gzfs = newGzFileStream("tests/files/test1.fastq.gz")
-  
-  while not gzfs.atEnd():
-    echo gzfs.readLine()
-    echo gzfs.readLine()
-    echo gzfs.readLine()
-    echo gzfs.readLine()
+  var f: FastQ
+  new(f)
+  f = parseFastQFile("tests/files/test1.fastq.gz")
+  echo f
