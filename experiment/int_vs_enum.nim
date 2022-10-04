042658c9735a300d090a76e1acbe0764b3cccdb4
Kerry Cobb <cobbkerry@gmail.com>
2022-10-04 17:07:31 -0500

    Tidied up int vs enum experiments and tried out -d:danger

--- experiment/int_vs_enum.nim
+++ experiment/int_vs_enum.nim
@@ -1,6 +1,6 @@
-# import sequtils
+# TODO: Why is -d:release so slow for equality int lookup? Should try more recent nim version then report.
+
 import times
-# import os
 import strutils
 import random
 
@@ -13,94 +13,125 @@ template benchmark(benchmarkName: string, code: untyped) =
     echo "CPU Time [", benchmarkName, "] ", elapsedStr, "s"
 
 type
-  DNA = enum
-    dnaA, dnaC, dnaG, dnaT 
+  DNA = enum dnaA, dnaG, dnaC, dnaT, dnaR, dnaM, dnaW, dnaS, dnaK, dnaY, dnaV, dnaH, dnaD, dnaB, dnaN, dnaGap, dnaUnk
 
 const
-  dnaByte: array[DNA, uint8] = [136'u8, 40, 72, 24] 
-  dnaChar: array[DNA, char] = ['A', 'C', 'G', 'T']
-  dnaComp: array[DNA, DNA] = [dnaT, dnaG, dnaC, dnaA]
+  dnaByte: array[DNA, uint8] = [0b1000_1000'u8, 0b0100_1000, 0b0010_1000, 0b0001_1000, 0b1100_0000, 0b1010_0000, 0b1001_0000, 0b0110_0000, 0b0101_0000, 0b0011_0000, 0b1110_0000, 0b1011_0000, 0b1101_0000, 0b0111_0000, 0b1111_0000, 0b0000_0100, 0b0000_0010]
+  dnaChar: array[DNA, char] = ['A','G','C','T','R','M','W','S','K','Y','V','H', 'D','B','N','-','?']
+  dnaComp: array[DNA, DNA] = [dnaT, dnaC, dnaG, dnaA, dnaY, dnaK, dnaW, dnaS, dnaM, dnaR, dnaB, dnaD, dnaH, dnaV, dnaN, dnaGap, dnaUnk]
 
 proc byte(dna: DNA): uint8 = dnaByte[dna]
 proc char(dna: DNA): char = dnaChar[dna]
 
 proc toChar(dna: DNA): char = 
   case dna 
-  of dnaA:
-    result = 'A'
-  of dnaG:
-    result = 'G'
-  of dnaC:
-    result = 'C'
-  of dnaT:
-    result = 'T'
+  of dnaA: result = 'A'
+  of dnaG: result = 'G'
+  of dnaC: result = 'C'
+  of dnaT: result = 'T'
+  of dnaR: result = 'R'
+  of dnaM: result = 'M'
+  of dnaW: result = 'W'
+  of dnaS: result = 'S'
+  of dnaK: result = 'K'
+  of dnaY: result = 'Y'
+  of dnaV: result = 'V'
+  of dnaH: result = 'H'
+  of dnaD: result = 'D'
+  of dnaB: result = 'B'
+  of dnaN: result = 'N'
+  of dnaGap: result = '-'
+  of dnaUnk: result = '?'
 
 # Benchmarks
 const
-  N = 1_000_000_000
+  N = 100_000_000
+echo "N: " & $N
 var 
   a:  array[N, DNA] 
   b:  array[N, uint8] 
 for i in 0 ..< N:
-  let d = DNA(rand(0..3))
+  let d = DNA(rand(0..16))
   a[i] = d 
   b[i] = d.byte 
 
 
-benchmark "as int":
-  var v: bool 
-  for rep in 0..int(1e3):
+let n1 = 1000
+echo "n1: " & $n1
+benchmark "equality int direct":
+  for rep in 0..n1:
     for i in b:
-      v = i == 136'u8
-      v = i == 72'u8
-      v = i == 40'u8
-      v = i == 24'u8
+      var b1 = i == 136'u8
+      var b2 = i == 72'u8
+      var b3 = i == 40'u8
+      var b4 = i == 24'u8
 
-benchmark "int lookup":
-  var v: bool
-  for rep in 0..int(1e3):
+benchmark "equality int lookup":
+  for rep in 0..n1:
     for i in a: 
-      v = i.byte == 136'u8
-      v = i.byte == 72'u8
-      v = i.byte == 40'u8
-      v = i.byte == 24'u8
+      var b1 = i.byte == 136'u8
+      var b2 = i.byte == 72'u8
+      var b3 = i.byte == 40'u8
+      var b4 = i.byte == 24'u8
 
-benchmark "as enum":
-  var v: bool
-  for rep in 0..int(1e3):
+benchmark "equality enum":
+  for rep in 0..n1:
     for i in a: 
-      v = i == dnaA  
-      v = i == dnaG
-      v = i == dnaC
-      v = i == dnaT
+      var b1 = i == dnaA  
+      var b2 = i == dnaG
+      var b3 = i == dnaC
+      var b4 = i == dnaT
 
-benchmark "enum assign":
-  var v: byte 
-  for rep in 0..int(1e11):
+
+let n2 = int(1e10)
+echo "n2: " & $n2
+benchmark "enum to int assignment":
+  for rep in 0..n2:
     for i in 0 ..< a.len:
-      v = a[i].byte
+       var v = a[i].byte
 
-benchmark "int assign":
-  var v: byte 
-  for rep in 0..int(1e11):
+benchmark "int to int assignment":
+  for rep in 0..n2:
     for i in 0 ..< b.len:
-      v = b[i]
+      var v = b[i]
 
-benchmark "char case":
-  for rep in 0..int(1e11):
+
+let n3 = int(100)
+echo "n3: " & $n3
+benchmark "char from case statment":
+  for rep in 0..n3:
     for i in a: 
-      discard i.toChar
+      var c = i.toChar
 
-benchmark "char lookup":
-  for rep in 0..int(1e11):
+benchmark "char from enum lookup":
+  for rep in 0..n3:
     for i in a: 
-      discard i.char
+      var c = i.char
 
+# Results
 
-# CPU Time [as int]      11.956s, 11.927s, 12.199s
-# CPU Time [int lookup]  11.959s, 11.937s, 11.901s
-# CPU Time [as enum]     11.917s, 11.915s, 11.910s
-# CPU Time [enum assign] 3.701s, 3.737s, 3.738s
-# CPU Time [int assign]  3.779s, 3.708s, 3.719s
-# CPU Time [char case]   3.744s, 3.734s, 3.740s
-# CPU Time [char lookup] 3.744s, 3.728s, 3.716s
+# nim c -r -d:release int_vs_enum.nim
+# N: 100000000
+# n1: 1000
+# CPU Time [equality int direct] 1.474s
+# CPU Time [equality int lookup] 1.434s
+# CPU Time [equality enum] 1.431s
+# n2: 10000000000
+# CPU Time [enum to int assignment] 0.448s
+# CPU Time [int to int assignment] 0.480s
+# n3: 100
+# CPU Time [char from case statment] 1.336s
+# CPU Time [char from enum lookup] 0.146s
+
+# nim c -r -d:danger int_vs_enum.nim
+# N: 100000000
+# n1: 1000
+# CPU Time [equality int direct] 1.269s
+# CPU Time [equality int lookup] 62.858s
+# CPU Time [equality enum] 1.203s
+# n2: 7766279631452241920
+# CPU Time [enum to int assignment] 0.000s
+# CPU Time [int to int assignment] 0.000s
+# n3: 100
+# CPU Time [char from case statment] 1.149s
+# CPU Time [char from enum lookup] 0.167s
