f0df1fea552f0b3d4c26418a6fa957993cdc74e4
Kerry Cobb <cobbkerry@gmail.com>
2022-10-04 17:05:09 -0500

    Changed approach to translating to amino acids

--- src/bioseq/aminoAcid.nim
+++ src/bioseq/aminoAcid.nim
@@ -6,6 +6,7 @@
 import ./nucleotide
 import ./parserMacro
 import std/macros
+import std/math
 
 ## The `aminoAcid` module contains the `AminoAcid` `enum` type for working with  
 ## amino acid sequence data. Using an enum type provides convenience and type safety.
@@ -42,10 +43,10 @@ import std/macros
 ## V    	 Val 	        Valine
 ## W    	 Trp 	        Tryptophan
 ## Y    	 Tyr 	        Tyrosine
-## \*      Stp          Stop
-## X       Amb          Ambiguous/Unknown 
 ## B       Asx          Aspartic acid or asparagine 
 ## Z       Glx          Glutamic acid or glutamine
+## \*      Stp          Stop
+## X       Amb          Ambiguous/Unknown 
 ## ======  ===========  ============================ 
 
 runnableExamples:
@@ -54,33 +55,36 @@ runnableExamples:
   let ala = parseChar('A', AminoAcid)  
   assert ala.char() == 'A'
 
-  let amino = translate([dnaT, dnaT, dnaT], gc_Standard_1)
+  let amino = translate([dnaT, dnaT, dnaT], gCode1)
   assert amino == aaF
 
 
 type
   AminoAcid* = enum aaA, aaC, aaD, aaE, aaF, aaG, aaH, aaI, aaK, aaL, aaM, aaN, 
-      aaO, aaP, aaQ, aaR, aaS, aaT, aaU, aaV, aaW, aaY, aaStp, aaX, aaB, aaZ
+      aaO, aaP, aaQ, aaR, aaS, aaT, aaU, aaV, aaW, aaY, aaB, aaZ, aaStp, aaX
 
 const
   aminoAcidChar*: array[AminoAcid, char] = ['A','C','D','E','F','G','H','I','K',
-      'L','M','N','O','P','Q','R','S','T','U','V','W','Y','*','X','B','Z']
+      'L','M','N','O','P','Q','R','S','T','U','V','W','Y','B','Z','*','X']
 
   aminoAcidAbrev*: array[AminoAcid, string] = ["Ala","Cys","Asp","Glu","Phe",
       "Gly","His","Ile","Lys","Leu","Met","Asn","Pyl","Pro","Gln","Arg","Ser",
-      "Thr","Sec","Val","Trp","Tyr","Stp","Amb","Asx","Glx"]
+      "Thr","Sec","Val","Trp","Tyr","Asx","Glx","Stp","Amb"]
 
   aminoAcidName*: array[AminoAcid, string] = ["Alanine","Cysteine",
       "Aspartic Acid","Glutamic Acid","Phenylalanine","Glycine","Histidine",
       "Isoleucine","Lysine","Leucine","Methionine","Asparagine","Pyrrolysine",
       "Proline","Glutamine","Arginine","Serine","Threonine","Selenocysteine",
-      "Valine","Tryptophan","Tyrosine","Stop","Ambiguous/Unknown",
-      "Aspartic acid or asparagine","Glutamic acid or glutamine"]
+      "Valine","Tryptophan","Tyrosine","Aspartic acid or asparagine",
+      "Glutamic acid or glutamine","Stop","Ambiguous/Unknown"]
 
 func parseChar*(c: char, T: typedesc[AminoAcid]): AminoAcid = 
   ## Parse character to DNA enum type.
   generateParser(c, aminoAcidChar, AminoAcid)
 
+func char*(a: AminoAcid): char = aminoAcidChar[a]
+  ## Character representation of amino acid.
+  ## 
 func toAminoAcidSeq*(data: seq[char]): seq[AminoAcid] =
   ## Parse character seq as Nucleotide seq
   result = newSeq[AminoAcid](data.len)
@@ -91,120 +95,168 @@ func toAminoAcidSeq*(data: string): seq[AminoAcid] =
   ## Parse string as Nucleotide seq 
   toAminoAcidSeq(cast[seq[char]](data))
 
-func char*(a: AminoAcid): char = aminoAcidChar[a]
-  ## Character representation of amino acid.
-
-const  
-  ## Codon tables
-  dnaCodonSequence* = [
-    "TTT","TTC","TTA","TTG","TCT","TCC","TCA","TCG",
-    "TAT","TAC","TAA","TAG","TGT","TGC","TGA","TGG",
-    "CTT","CTC","CTA","CTG","CCT","CCC","CCA","CCG",
-    "CAT","CAC","CAA","CAG","CGT","CGC","CGA","CGG",
-    "ATT","ATC","ATA","ATG","ACT","ACC","ACA","ACG",
-    "AAT","AAC","AAA","AAG","AGT","AGC","AGA","AGG",
-    "GTT","GTC","GTA","GTG","GCT","GCC","GCA","GCG",
-    "GAT","GAC","GAA","GAG","GGT","GGC","GGA","GGG"]
-    
-  rnaCodonSequence* = [
-    "UUU","UUC","UUA","UUG","UCU","UCC","UCA","UCG",
-    "UAU","UAC","UAA","UAG","UGU","UGC","UGA","UGG",
-    "CUU","CUC","CUA","CUG","CCU","CCC","CCA","CCG",
-    "CAU","CAC","CAA","CAG","CGU","CGC","CGA","CGG",
-    "AUU","AUC","AUA","AUG","ACU","ACC","ACA","ACG",
-    "AAU","AAC","AAA","AAG","AGU","AGC","AGA","AGG",
-    "GUU","GUC","GUA","GUG","GCU","GCC","GCA","GCG",
-    "GAU","GAC","GAA","GAG","GGU","GGC","GGA","GGG"]
+# TODO: Are these useful for anything?
+# const
+#   dnaCodons = [
+#     "AAA", "AAG", "AAC", "AAT", "AGA", "AGG", "AGC", "AGT", 
+#     "ACA", "ACG", "ACC", "ACT", "ATA", "ATG", "ATC", "ATT", 
+#     "GAA", "GAG", "GAC", "GAT", "GGA", "GGG", "GGC", "GGT",
+#     "GCA", "GCG", "GCC", "GCT", "GTA", "GTG", "GTC", "GTT", 
+#     "CAA", "CAG", "CAC", "CAT", "CGA", "CGG", "CGC", "CGT", 
+#     "CCA", "CCG", "CCC", "CCT", "CTA", "CTG", "CTC", "CTT", 
+#     "TAA", "TAG", "TAC", "TAT", "TGA", "TGG", "TGC", "TGT", 
+#     "TCA", "TCG", "TCC", "TCT", "TTA", "TTG", "TTC", "TTT"]
+#   rnaCodons = [
+#     "AAA", "AAG", "AAC", "AAU", "AGA", "AGG", "AGC", "AGU", 
+#     "ACA", "ACG", "ACC", "ACU", "AUA", "AUG", "AUC", "AUU", 
+#     "GAA", "GAG", "GAC", "GAU", "GGA", "GGG", "GGC", "GGU",
+#     "GCA", "GCG", "GCC", "GCU", "GUA", "GUG", "GUC", "GUU", 
+#     "CAA", "CAG", "CAC", "CAU", "CGA", "CGG", "CGC", "CGU", 
+#     "CCA", "CCG", "CCC", "CCU", "CUA", "CUG", "CUC", "CUU", 
+#     "UAA", "UAG", "UAC", "UAU", "UGA", "UGG", "UGC", "UGU", 
+#     "UCA", "UCG", "UCC", "UCU", "UUA", "UUG", "UUC", "UUU"]
 
 type
   GeneticCode* = enum 
-    ## Genetic codes for translating nucleotides to amino acids. See source for more information.
-    ## Source: https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi
-    gc_Standard_1 = "FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Vertebrate_Mito_2 = "FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSS**VVVVAAAADDEEGGGG",
-    gc_Yeast_Mito_3 = "FFLLSSSSYY**CCWWTTTTPPPPHHQQRRRRIIMMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Mold_Protozoan_Coelenterate_Mito_and_Mycoplasma_Spiroplasma_4 = "FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Invertebrate_Mito_5 = "FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSSSSVVVVAAAADDEEGGGG",
-    gc_Ciliate_Dasycladacean_and_Hexamita_Nuc_6 = "FFLLSSSSYYQQCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Echinoderm_and_Flatworm_Mito_9 = "FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNNKSSSSVVVVAAAADDEEGGGG",
-    gc_Euplotid_Nuc_10 = "FFLLSSSSYY**CCCWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Bacterial_Archaeal_and_Plant_Plastid_11 = "FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Alternative_Yeast_Nuc_12 = "FFLLSSSSYY**CC*WLLLSPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Ascidian_Mito_13 = "FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSSGGVVVVAAAADDEEGGGG",
-    gc_Alternative_Flatworm_Mito_14 = "FFLLSSSSYYY*CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNNKSSSSVVVVAAAADDEEGGGG",
-    gc_Chlorophycean_Mito_16 = "FFLLSSSSYY*LCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Trematode_Mito_21 = "FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNNKSSSSVVVVAAAADDEEGGGG",
-    gc_Scenedesmus_obliquus_Mito_22 = "FFLLSS*SYY*LCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Thraustochytrium_Mito_23 = "FF*LSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Rhabdopleuridae_Mito_24 = "FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSSKVVVVAAAADDEEGGGG",
-    gc_Candidate_Division_SR1_and_Gracilibacteria_25 = "FFLLSSSSYY**CCGWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Pachysolen_tannophilus_Nuc_26 = "FFLLSSSSYY**CC*WLLLAPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Karyorelict_Nuc_27 = "FFLLSSSSYYQQCCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Condylostoma_Nuc_28 = "FFLLSSSSYYQQCCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Mesodinium_Nuc_29 = "FFLLSSSSYYYYCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Peritrich_Nuc_30 = "FFLLSSSSYYEECC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Blastocrithidia_Nuc_31 = "FFLLSSSSYYEECCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG",
-    gc_Cephalodiscidae_Mito_UAA_Tyr_33 = "FFLLSSSSYYY*CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSSKVVVVAAAADDEEGGGG"
+    ## Genetic codes for translating nucleotides to amino acids. 
+    ## 
+    ## The genetic code follows the `NCBI definitions <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi>`_ 
+    ## 
+    ## gCode1  - `The Standard Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG1>`_
+    ## gCode2  - `The Vertebrate Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG2>`_
+    ## gCode3  - `The Yeast Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG3>`_
+    ## gCode4  - `The Mold, Protozoan, and Coelenterate Mitochondrial Code and the Mycoplasma/Spiroplasma Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG4>`_
+    ## gCode5  - `The Invertebrate Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG5>`_
+    ## gCode6  - `The Ciliate, Dasycladacean and Hexamita Nuclear Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG6>`_
+    ## gCode9  - `The Echinoderm and Flatworm Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG9>`_
+    ## gCode10 - `The Euplotid Nuclear Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG10>`_
+    ## gCode11 - `The Bacterial, Archaeal and Plant Plastid Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG11>`_
+    ## gCode12 - `The Alternative Yeast Nuclear Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG12>`_
+    ## gCode13 - `The Ascidian Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG13>`_
+    ## gCode14 - `The Alternative Flatworm Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG14>`_
+    ## gCode16 - `Chlorophycean Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG16>`_
+    ## gCode21 - `Trematode Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG21>`_
+    ## gCode22 - `Scenedesmus obliquus Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG22>`_
+    ## gCode23 - `Thraustochytrium Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG23>`_
+    ## gCode24 - `Rhabdopleuridae Mitochondrial Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG24>`_
+    ## gCode25 - `Candidate Division SR1 and Gracilibacteria Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG25>`_
+    ## gCode26 - `Pachysolen tannophilus Nuclear Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG26>`_
+    ## gCode27 - `Karyorelict Nuclear Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG27>`_
+    ## gCode28 - `Condylostoma Nuclear Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG28>`_
+    ## gCode29 - `Mesodinium Nuclear Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG29>`_
+    ## gCode30 - `Peritrich Nuclear Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG30>`_
+    ## gCode31 - `Blastocrithidia Nuclear Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG31>`_
+    ## gCode33 - `Cephalodiscidae Mitochondrial UAA-Tyr Code <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi#SG33>`_
+    ##
+    ## A more descriptive alias is available for each genetic code if desired: 
+    ## gCode1: `gc_Standard`
+    ## gCode2: `gc_Vertebrate_Mito`
+    ## gCode3: `gc_Yeast_Mito`
+    ## gCode4: `gc_Mold_Protozoan_Coelenterate_Mito_and_Mycoplasma_Spiroplasma`
+    ## gCode5: `gc_Invertebrate_Mito`
+    ## gCode6: `gc_Ciliate_Dasycladacean_and_Hexamita_Nuc`
+    ## gCode9: `gc_Echinoderm_and_Flatworm_Mito`
+    ## gCode10: `gc_Euplotid_Nuc`
+    ## gCode11: `gc_Bacterial_Archaeal_and_Plant_Plastid`
+    ## gCode12: `gc_Alternative_Yeast_Nuc`
+    ## gCode13: `gc_Ascidian_Mito`
+    ## gCode14: `gc_Alternative_Flatworm_Mito`
+    ## gCode16: `gc_Chlorophycean_Mito`
+    ## gCode21: `gc_Trematode_Mito`
+    ## gCode22: `gc_Scenedesmus_obliquus_Mito`
+    ## gCode23: `gc_Thraustochytrium_Mito`
+    ## gCode24: `gc_Rhabdopleuridae_Mito`
+    ## gCode25: `gc_Candidate_Division_SR1_and_Gracilibacteria`
+    ## gCode26: `gc_Pachysolen_tannophilus_Nuc`
+    ## gCode27: `gc_Karyorelict_Nuc`
+    ## gCode28: `gc_Condylostoma_Nuc`
+    ## gCode29: `gc_Mesodinium_Nuc`
+    ## gCode30: `gc_Peritrich_Nuc`
+    ## gCode31: `gc_Blastocrithidia_Nuc`
+    ## gCode33: `gc_Cephalodiscidae_Mito_UAA_Tyr` 
+    ## 
+    ## The amino acids in each string are ordered relative to this sequence of codons:
+    ##  AAA, AAG, AAC, AAT, AGA, AGG, AGC, AGT, ACA, ACG, ACC, ACT, ATA, ATG, 
+    ##    ATC, ATT, GAA, GAG, GAC, GAT, GGA, GGG, GGC, GGT, GCA, GCG, GCC, GCT, 
+    ##    GTA, GTG, GTC, GTT, CAA, CAG, CAC, CAT, CGA, CGG, CGC, CGT, CCA, CCG, 
+    ##    CCC, CCT, CTA, CTG, CTC, CTT, TAA, TAG, TAC, TAT, TGA, TGG, TGC, TGT, 
+    ##    TCA, TCG, TCC, TCT, TTA, TTG, TTC, TTT
+    
+    gCode1  = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YY*WCCSSSSLLFF",
+    gCode2  = "KKNN**SSTTTTMMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YYWWCCSSSSLLFF",
+    gCode3  = "KKNNRRSSTTTTMMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPTTTT**YYWWCCSSSSLLFF",
+    gCode4  = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YYWWCCSSSSLLFF",
+    gCode5  = "KKNNSSSSTTTTMMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YYWWCCSSSSLLFF",
+    gCode6  = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLLQQYY*WCCSSSSLLFF",
+    gCode9  = "NKNNSSSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YYWWCCSSSSLLFF",
+    gCode10 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YYCWCCSSSSLLFF",
+    gCode11 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YY*WCCSSSSLLFF",
+    gCode12 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLSLL**YY*WCCSSSSLLFF",
+    gCode13 = "KKNNGGSSTTTTMMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YYWWCCSSSSLLFF",
+    gCode14 = "NKNNSSSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLLY*YYWWCCSSSSLLFF",
+    gCode16 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL*LYY*WCCSSSSLLFF",
+    gCode21 = "NKNNSSSSTTTTMMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YYWWCCSSSSLLFF",
+    gCode22 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL*LYY*WCC*SSSLLFF",
+    gCode23 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YY*WCCSSSS*LFF",
+    gCode24 = "KKNNSKSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YYWWCCSSSSLLFF",
+    gCode25 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLL**YYGWCCSSSSLLFF",
+    gCode26 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLALL**YY*WCCSSSSLLFF",
+    gCode27 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLLQQYYWWCCSSSSLLFF",
+    gCode28 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLLQQYYWWCCSSSSLLFF",
+    gCode29 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLLYYYY*WCCSSSSLLFF",
+    gCode30 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLLEEYY*WCCSSSSLLFF",
+    gCode31 = "KKNNRRSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLLEEYYWWCCSSSSLLFF",
+    gCode33 = "KKNNSKSSTTTTIMIIEEDDGGGGAAAAVVVVQQHHRRRRPPPPLLLLY*YYWWCCSSSSLLFF",
 
+const 
+  gc_Standard* = gCode1
+  gc_Vertebrate_Mito* = gCode2
+  gc_Yeast_Mito* = gCode3
+  gc_Mold_Protozoan_Coelenterate_Mito_and_Mycoplasma_Spiroplasma* = gCode4
+  gc_Invertebrate_Mito* = gCode5
+  gc_Ciliate_Dasycladacean_and_Hexamita_Nuc* = gCode6
+  gc_Echinoderm_and_Flatworm_Mito* = gCode9
+  gc_Euplotid_Nuc* = gCode10
+  gc_Bacterial_Archaeal_and_Plant_Plastid* = gCode11
+  gc_Alternative_Yeast_Nuc* = gCode12
+  gc_Ascidian_Mito* = gCode13
+  gc_Alternative_Flatworm_Mito* = gCode14
+  gc_Chlorophycean_Mito* = gCode16
+  gc_Trematode_Mito* = gCode21
+  gc_Scenedesmus_obliquus_Mito* = gCode22
+  gc_Thraustochytrium_Mito* = gCode23
+  gc_Rhabdopleuridae_Mito* = gCode24
+  gc_Candidate_Division_SR1_and_Gracilibacteria* = gCode25
+  gc_Pachysolen_tannophilus_Nuc* = gCode26
+  gc_Karyorelict_Nuc* = gCode27
+  gc_Condylostoma_Nuc* = gCode28
+  gc_Mesodinium_Nuc* = gCode29
+  gc_Peritrich_Nuc* = gCode30
+  gc_Blastocrithidia_Nuc* = gCode31
+  gc_Cephalodiscidae_Mito_UAA_Tyr* = gCode33
 
-macro generateTranslation(nucleotides: array[3, Nucleotide], aminoAcids: string): untyped = 
-  # TODO: Raise exception if codon includes gaps 
-  # TODO: Change this to use the sum of the ordinals instead of converting to string 
-  # which would make the macro unnecessary.
-  ## Generates case statement for translating Amino acid codon sequence
-  ## 
-  ## Generated code:
-  ## 
-  ## var str = newString(3)
-  ## for i in 0 .. 2:
-  ##   str[i] = nucleotides[i].char
-  ## case str
-  ## of dnaCodonSequence[0]:
-  ##   result = parseChar(standard[0], AminoAcid)
-  ## ...
-  ## else:
-  ##   result = aaX 
-  result = nnkStmtList.newTree()
-  # Determnine if nucleotides are RNA or DNA and select appropriate Codon seuqences
-  var codons: array[64, string]
-  if nucleotides.getTypeInst.repr == "array[0 .. 2, DNA]":
-    codons = dnaCodonSequence 
-  elif nucleotides.getTypeInst.repr == "array[0 .. 2, RNA]":
-    codons = rnaCodonSequence 
-  # Generate code to convert array of nucleotides into 3 character string
-  result.add(
-    nnkVarSection.newTree(
-      nnkIdentDefs.newTree(newIdentNode("str"), newEmptyNode(),  
-          nnkCall.newTree(newIdentNode("newString"), newLit(3)))),
-    nnkForStmt.newTree(
-      newIdentNode("i"),
-      nnkInfix.newTree(newIdentNode(".."), newLit(0), newLit(2)),
-      nnkStmtList.newTree(
-        nnkAsgn.newTree(
-          nnkBracketExpr.newTree(newIdentNode("str"), newIdentNode("i")),
-          nnkDotExpr.newTree(nnkBracketExpr.newTree(nucleotides, 
-              newIdentNode("i")), newIdentNode("char"))))))
-  # Generate case statement
-  var caseStmt = nnkCaseStmt.newTree(newIdentNode("str"))
-  # For each codon assign the correct amino acid
-  for i in 0 .. 63:
-    caseStmt.add(
-      nnkOfBranch.newTree(
-        newLit(codons[i]),
-        nnkStmtList.newTree(
-          nnkAsgn.newTree(
-            newIdentNode("result"),
-            nnkCall.newTree(newIdentNode("parseChar"), 
-                nnkBracketExpr.newTree(aminoAcids, newIntLitNode(i)), 
-                newIdentNode("AminoAcid"))))))
-  # If the string doesn't match a valid codon then assign unknown amino acid
-  caseStmt.add(  
-    nnkElse.newTree(
-      nnkAsgn.newTree(newIdentNode("result"), newLit(aaX))
-  ))
-  result.add(caseStmt) 
-  # echo result.repr
+macro createGeneticCodeMatrix(): untyped = 
+  ## Creates 2D array from GeneticCode strings for easy indexing by the 
+  ## translate proc.
+  var matrix: array[GeneticCode.high.ord * 64, AminoAcid]
+  for i in 0 ..< GeneticCode.high.ord:
+    for j in 0 ..< 64:
+      let aminoAcidChar = ($GeneticCode(i))[j] 
+      matrix[i * 64 + j] = parseChar(aminoAcidChar, AminoAcid)
+  result = newLit(matrix)
+  # # Alternative which would be called inside of translate() proc
+  # result = nnkStmtList.newTree(nnkLetSection.newTree(nnkIdentDefs.newTree(
+  #   newIdentNode("geneticCodeMatrix"), newEmptyNode(), newLit(matrix))))
+
+const geneticCodeMatrix = createGeneticCodeMatrix() 
 
 func translate*(nucleotides: array[3, AnyNucleotide], code: GeneticCode): AminoAcid = 
-  ## Translate nucleotide codo to amino acid
-  generateTranslation(nucleotides, $code) 
+  ## Translate nucleotide codon to amino acid. 
+  var num = 0
+  for i in 0..2:
+    let p = 4^(2-i) 
+    num += p * nucleotides[i].ord   
+  if num < 64:
+    result = geneticCodeMatrix[code.ord * 64 + num]  
+  else:
+    result = aaX 
