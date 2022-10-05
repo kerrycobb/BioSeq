0f37df55b7b6330bcab75722ec3569f893471a5b
Kerry Cobb <cobbkerry@gmail.com>
2022-10-05 10:00:27 -0500

    Cleaning up documentation

--- src/bioseq.nim
+++ src/bioseq.nim
@@ -16,18 +16,18 @@ export
 ## RNA, and amino acids. 
 ## 
 ## BioSeq modules:
-## `nucleotide <bioseq/nucleotide.html>`_
+## `nucleotide<./bioseq/nucleotide.html>`_
 ##    Provides functions for working with nucleotides such as: 
 ##      - Safe parsing
 ##      - Getting nucleotide complement
 ##      - Fast ambiguous nucleic acid comparison 
 ## 
-## `aminoAcid <bioseq/aminoAcid.html>`_
+## `aminoAcid<./bioseq/aminoAcid.html>`_
 ##    Provides functions for working with amino acids such as: 
 ##      - Safe parsing 
 ##      - Translating from amino acid sequence 
 ## 
-## `twoBitSequence <bioseq/twoBitSequence>`_
+## `twoBitSequence <./bioseq/twoBitSequence>`_
 ##    Provides storage of nucleotides sequence as a pair of bits within a 
 ##    bit sequence which reduces memory and allows for faster performance with 
 ##    certain sequence comparisons. 
