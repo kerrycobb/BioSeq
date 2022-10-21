e814b0869f1c0c8eb311fae31764a2302f3432ec
Kerry Cobb <cobbkerry@gmail.com>
2022-10-21 12:04:27 -0500

    Improved tests and added some for iterators

--- tests/test_phylip.nim
+++ tests/test_phylip.nim
@@ -6,15 +6,17 @@
 import ../src/bioseq
 import std/unittest 
 import std/strutils
+import std/enumerate
 
-suite "Phylip Reading":
+suite "Single Phylip":
   let 
-    expectedIds =  @["Sample1", "Sample2", "Sample3", "Sample4"]
-    expectedData = @[
+    ids =  @["Sample1", "Sample2", "Sample3", "Sample4"]
+    data = @[
         "ATGCATGCATGC", 
         "TTGCTTGCATGC", 
         "GTGCGTGCATGC", 
-        "CTGCCTGCATGC"].join()
+        "CTGCCTGCATGC"].join().toNucleotideSeq(DNA)
+    expected = newAlignment[DNA](4, 12, ids, data)
 
   test "Interleaved String":    
     let 
@@ -41,10 +43,7 @@ suite "Phylip Reading":
 
         """
       a = parsePhylipString(str, DNA, Interleaved)
-    check a.ids == expectedIds
-    check a.data.toString.replace("\n", "") == expectedData 
-    check a.nseqs == 4
-    check a.nchars == 12
+    check a == expected 
 
   test "Sequential String":
     let 
@@ -77,7 +76,64 @@ suite "Phylip Reading":
 
         """.dedent()
       a = parsePhylipString(str, DNA, Sequential)
-    check a.ids == expectedIds
-    check a.data.toString.replace("\n", "") == expectedData 
-    check a.nseqs == 4
-    check a.nchars == 12
+    check a == expected
+
+
+
+suite "Multiple Phylip":    
+  let 
+    expected = @[
+      newAlignment[DNA](2, 8, @["Sample1", "Sample2"], 
+          @["ATGCATGC", "TTGCATGC"].join().toNucleotideSeq(DNA)),
+      newAlignment[DNA](3, 7, @["Sample3", "Sample4", "Sample5"], 
+          @["GTGCATG", "CTGCATG", "ATGCATG"].join().toNucleotideSeq(DNA)),
+    ] 
+    interleaved = """
+      2 8
+      Sample1 ATGC
+      Sample2 TTGC
+      ATGC
+      ATGC
+      3 7 
+      Sample3 GTGC
+      Sample4 CTGC
+      Sample5 ATGC
+      ATG
+      ATG
+      ATG
+      """
+    sequential = """
+      2 8
+      Sample1 ATGC
+      ATGC
+      Sample2 TTGC
+      ATGC
+      3 7 
+      Sample3 GTGC
+      ATG
+      Sample4 CTGC
+      ATG
+      Sample5 ATGC
+      ATG
+      """
+
+  suite "Read Single From Multiple":
+
+    test "Interleaved":
+      let a = parsePhylipString(interleaved, DNA, Interleaved)
+      check a == expected[0]
+
+    test "Sequential":
+      let a = parsePhylipString(sequential, DNA, Sequential)
+      check a == expected[0]
+
+  suite "Phylip Iterators":
+    
+    test "Interleaved Iterator":
+      for i, a in enumerate(iterPhylipString(interleaved, DNA, Interleaved)):
+        check a == expected[i]
+
+    test "Sequential Iterator":
+      for i, a in enumerate(iterPhylipString(sequential, DNA, Sequential)):
+        check a == expected[i]
+
