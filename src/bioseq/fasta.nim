58c5f30b327a42967e0a8122d6ca33a7a5b9359a
Kerry Cobb <cobbkerry@gmail.com>
2022-10-24 13:11:36 -0500

    Added Fasta writing procs

--- src/bioseq/fasta.nim
+++ src/bioseq/fasta.nim
@@ -68,13 +68,56 @@ iterator iterFastaStream*(stream: Stream, typ: typedesc): SeqRecord[typ] =
   yield sequence
 
 iterator iterFastaString*(str: string, typ: typedesc): SeqRecord[typ] =  
+  ## Iterate over records in a Fasta string 
   var ss = newStringStream(str)
   for i in iterFastaStream(ss, typ):
     yield i
   ss.close
 
 iterator iterFastaFile*(path: string, typ: typedesc): SeqRecord[typ] =  
+  ## Iterate over records in a Fasta file
   var fs = newFileStream(path)
   for i in iterFastaStream(fs, typ):
     yield i
   fs.close
+
+# import std/math
+
+proc toFastaString*[T](s: SeqRecord[T], lineLength = 80): string = 
+  ## Returns string in Fasta format
+  var 
+    nLines = (s.data.len + lineLength - 1) div lineLength 
+  result = newStringOfCap(s.id.len + s.data.len + nLines + 1) 
+  result.add('>')
+  result.add(s.id)
+  result.add('\n')
+  var endIx: int
+  for i in 0 ..< nLines - 1:
+    let startIx = i * lineLength 
+    endIx = startIx + lineLength
+    for j in startIx ..< endIx:
+      result.add(s.data[j].toChar)
+    result.add('\n')
+  for j in endIx ..< s.data.len:
+    result.add(s.data[j].toChar)
+  result.add('\n')
+
+proc toFastaString*[T](s: seq[SeqRecord[T]], lineLength = 80): string = 
+  ## Returns string in Fasta format
+  for i in s:
+    result.add(i.toFastaString)
+
+proc toFastaFile*[T](s: SeqRecord[T], path: string, mode: FileMode = fmWrite, 
+  # Writes data to file in Fasta format. Use fmAppend to append rather than overwrite.
+    lineLength = 80) =  
+  var fh = open(path, mode)
+  fh.write(s.toFastaString, lineLength)
+  fh.close
+
+proc toFastaFile*[T](s: seq[SeqRecord[T]], path: string, mode: FileMode = fmWrite, 
+  # Writes data to file in Fasta format. Use fmAppend to append rather than overwrite.
+    lineLength = 80) = 
+  var fh = open(path, mode)
+  for i in s:
+    fh.write(i.toFastaString, lineLength)
+  fh.close
